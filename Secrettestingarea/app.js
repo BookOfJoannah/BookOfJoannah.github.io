d8e812b0625f0af98be46a10124d9a2306ddb837
BookOfJoannah <pimasterj314@gmail.com>
2024-08-03 00:07:52 -0700

    screw it, we're just copying code
    let's copy whatever seems needed and see if it runs :3

--- Secrettestingarea/app.js
+++ Secrettestingarea/app.js
@@ -1,19 +1,28 @@
-var randomtext = angular.module('shortyism_generator', []);
+// Basically, this simple Angular controller pulls at random from an array of
+// "Shortyisms" every time the moar_shorty function is called via button click,
+// and sets $scope.shortyism to the string that is pulled. It also removes that
+// particular shortyism from the array of shortyisms and stores it in a second
+// array. If the controller detects that the original shortyism is empty, it
+// refills it from the array of previously used shortyisms. This minimizes the
+// repetition of "shortyisms" while still allowing the button to be pushed
+// indefinitely.
 
-randomtext.controller('cant_control_shorty', function($scope) {
+var app = angular.module('shortyism_generator', []);
+
+app.controller('cant_control_shorty', function($scope) {
     // Default value before a shortyism is called.
-    $scope.shortyism = "Will this be a default value?";
+    $scope.shortyism = "Click Below for Pearls of Techy Wisdom";
     $scope.moar_shorty = function() {
       // if the array of shortyisms is emtpy, refill it.
-      if (texts_A.length === 0) {
-        texts_A = texts_B;
-        texts_B = [];
+      if (shortyisms_A.length === 0) {
+        shortyisms_A = shortyisms_B;
+        shortyisms_B = [];
       }
-      select = Math.floor(Math.random()*texts_A.length);
-      $scope.shortyism = texts_A[select];
+      select = Math.floor(Math.random()*shortyisms_A.length);
+      $scope.shortyism = shortyisms_A[select];
       // store the now-used shortyism in another array, and remove it so it
       // doesn't repeat
-      texts_B.push(texts_A[select]);
-      texts_A.splice(select, 1);
+      shortyisms_B.push(shortyisms_A[select]);
+      shortyisms_A.splice(select, 1);
     };
 });
---
+++ Secrettestingarea/shortyisms.js
@@ -0,0 +1,21 @@
+var shortyisms_B = [];
+
+
+var shortyisms_A = [
+
+    "I covet other people's fancy pants.  I really love seeing people wearing \
+    really nice weird pants.  Not like khaki, or chinos, but like those people \
+    who get custom made pants and they're super unique.  I lose my lid on that \
+    shit!  I think that's great.  I think finally I'm going to to get some \
+    custom pants made so I maybe I won't have to covet anymore.",
+  
+    "Dance, Satan, dance!",
+  
+    "Stanley’s like you modafocka!",
+  
+    "Grass tastes terrible.",
+  
+    "It’s filthy. It’s dirty. It’s SO GOOD.",
+  
+    "damn you, autocomplete",
+];  
