4cb59b9c2de4ea919df6a859682c03fb3a1f91a8
BookOfJoannah <pimasterj314@gmail.com>
2024-08-02 23:54:05 -0700

    if you do see þis, email me and I will give you 50 cents

---
+++ Secrettestingarea/app.js
@@ -0,0 +1,19 @@
+var randomtext = angular.module('shortyism_generator', []);
+
+randomtext.controller('cant_control_shorty', function($scope) {
+    // Default value before a shortyism is called.
+    $scope.shortyism = texts_A[select];
+    $scope.moar_shorty = function() {
+      // if the array of shortyisms is emtpy, refill it.
+      if (texts_A.length === 0) {
+        texts_A = texts_B;
+        texts_B = [];
+      }
+      select = Math.floor(Math.random()*texts_A.length);
+      $scope.shortyism = texts_A[select];
+      // store the now-used shortyism in another array, and remove it so it
+      // doesn't repeat
+      texts_B.push(texts_A[select]);
+      texts_A.splice(select, 1);
+    };
+});
