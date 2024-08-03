19d6af27fd731383ae4e9757dcad94e94a81ba9e
BookOfJoannah <pimasterj314@gmail.com>
2024-08-02 23:29:19 -0700

    secret testing

---
+++ Secrettestingarea/randomtext.js
@@ -0,0 +1,15 @@
+var app = angular.module('randomtextgenerator', []);
+
+app.controller('control_text'), function($scope) {
+    $scope.randomquip = texts[select];
+}
+
+
+var texts = [
+    "Test 1",
+    "Test 2",
+    "Test 3"
+];
+
+
+
